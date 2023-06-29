51c6d3fa1aa54eff1319bbaf7609aaba0f63eedd
Marina Grigoreva <mgrigoreva12@gmail.com>
2023-06-29 12:09:52 +0300

    Add functions for scrolling

--- js/script.js
+++ js/script.js
@@ -1,16 +1,66 @@
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother, ScrollToPlugin);
 
+function fadeOut(element, start, end) {
+  gsap.fromTo(
+    element,
+    { opacity: 1 },
+    {
+      opacity: 0,
+      scrollTrigger: {
+        trigger: element,
+        start: `${start}`,
+        end: `${end}`,
+        scrub: true,
+      },
+    }
+  );
+}
+
+function slideTo(element, to, start, end) {
+  gsap.fromTo(
+    element,
+    { x: to },
+    {
+      x: 0,
+      scrollTrigger: {
+        trigger: element,
+        start: `${start}`,
+        end: `${end}`,
+        scrub: true,
+      },
+    }
+  );
+}
+
+function slideFrom(element, from, start, end) {
+  gsap.fromTo(
+    element,
+    { x: from, opacity: 0 },
+    {
+      x: 0,
+      opacity: 1,
+      scrollTrigger: {
+        trigger: element,
+        start: `${start}`,
+        end: `${end}`,
+        scrub: true,
+      },
+    }
+  );
+}
+
 // Smooth scroller
-ScrollSmoother.create({
-  smooth: 1.5,
-  effects: true,
-});
+if (ScrollTrigger.isTouch !== 1) {
+  ScrollSmoother.create({
+    smooth: 1.5,
+    effects: true,
+  });
+}
 
-// Navigation links
 document.addEventListener("DOMContentLoaded", function () {
+  // Navigation links
   const navLinks = document.querySelectorAll(".nav-link");
-
-  navLinks.forEach(function (link) {
+  navLinks.forEach((link) => {
     link.addEventListener("click", function (event) {
       event.preventDefault();
 
@@ -24,194 +74,106 @@ document.addEventListener("DOMContentLoaded", function () {
         scrollTo: { y: target, offsetY: 100 },
       });
     });
+
+    const logoLinks = document.querySelectorAll(".logo");
+    logoLinks.forEach((link) => {
+      link.addEventListener("click", (event) => {
+        event.preventDefault();
+        gsap.to(window, {
+          duration: 0.5,
+          scrollTo: { y: 0 },
+        });
+      });
+    });
   });
-});
 
-// Intro section
-gsap.fromTo(
-  ".intro",
-  { opacity: 1 },
-  {
-    opacity: 0,
-    scrollTrigger: {
-      trigger: ".intro",
-      start: "-50",
-      end: "300",
-      scrub: true,
-    },
-  }
-);
+  fadeOut(".intro", -50, 300);
 
-// Titles h2
-const titles = document.querySelectorAll(".title");
-titles.forEach((title) => {
-  gsap.fromTo(
-    title,
-    { x: -400 },
-    {
-      x: 0,
-      scrollTrigger: {
-        trigger: title,
-        start: "-850",
-        end: "-150",
-        scrub: true,
-      },
-    }
+  const titles = document.querySelectorAll(".title");
+  titles.forEach((title) => slideTo(title, -400, -850, -150));
+
+  const smoothHeadings = document.querySelectorAll(".smooth-headings");
+  smoothHeadings.forEach((el) => fadeOut(el, -100, 100));
+
+  const smoothTexts = document.querySelectorAll(".smooth-text");
+  smoothTexts.forEach((el) => slideFrom(el, 1000, -950, -300));
+
+  // Projects
+  const itemsLeft = document.querySelectorAll(
+    ".projects-gallery-left .projects-gallery-item"
   );
-});
+  itemsLeft.forEach((el) => slideFrom(el, -50, -900, -200));
 
-const smoothHeadings = gsap.utils.toArray(".smooth-headings");
-smoothHeadings.forEach((el) => {
-  gsap.fromTo(
-    el,
-    { opacity: 1 },
-    {
-      opacity: 0,
-      scrollTrigger: {
-        trigger: el,
-        start: "-100",
-        end: "100",
-        scrub: true,
-      },
-    }
+  const itemsRight = document.querySelectorAll(
+    ".projects-gallery-right .projects-gallery-item"
   );
-});
+  itemsRight.forEach((el) => slideFrom(el, 50, -900, -200));
 
-const smoothTexts = gsap.utils.toArray(".smooth-text");
-smoothTexts.forEach((el) => {
-  gsap.fromTo(
-    el,
-    { x: 1000, opacity: 0 },
-    {
-      x: 0,
-      opacity: 1,
-      scrollTrigger: {
-        trigger: el,
-        start: "-950",
-        end: "-300",
-        scrub: true,
-      },
+  // Slider
+  const previous = document.querySelector(".slider-btn-previous");
+  const next = document.querySelector(".slider-btn-next");
+  const images = document.querySelectorAll(".slider-img");
+  const slider = document.querySelector(".slider-images");
+  const totalImages = images.length;
+  const imageWidth = images[0].clientWidth;
+  let currentPosition = 0;
+  let touchStartX = 0;
+
+  function swapImages(direction) {
+    if (direction === "next") {
+      currentPosition -= imageWidth;
+    } else if (direction === "previous") {
+      currentPosition += imageWidth;
     }
-  );
-});
 
-// Projects
-const itemsLeft = gsap.utils.toArray(
-  ".projects-gallery-left .projects-gallery-item"
-);
-itemsLeft.forEach((item) => {
-  gsap.fromTo(
-    item,
-    { x: -50, opacity: 0 },
-    {
-      opacity: 1,
-      x: 0,
-      scrollTrigger: {
-        trigger: item,
-        start: "-900",
-        end: "-50",
-        scrub: true,
-      },
+    const lastImage =
+      window.innerWidth <= 768 ? totalImages - 1 : totalImages - 3;
+    const loopBackPosition = -(lastImage * imageWidth);
+
+    if (currentPosition < loopBackPosition) {
+      currentPosition = 0;
+    } else if (currentPosition > 0) {
+      currentPosition = loopBackPosition;
     }
-  );
-});
 
-const itemsRight = gsap.utils.toArray(
-  ".projects-gallery-right .projects-gallery-item"
-);
-itemsRight.forEach((item) => {
-  gsap.fromTo(
-    item,
-    { x: 50, opacity: 0 },
-    {
-      opacity: 1,
-      x: 0,
-      scrollTrigger: {
-        trigger: item,
-        start: "-900",
-        end: "-150",
-        scrub: true,
-      },
-    }
-  );
-});
-
-// Slider
-const previous = document.querySelector(".slider-btn-previous");
-const next = document.querySelector(".slider-btn-next");
-const images = document.querySelectorAll(".slider-img");
-const slider = document.querySelector(".slider-images");
-const totalImages = images.length;
-const imageWidth = images[0].clientWidth;
-let currentPosition = 0;
-let touchStartX = 0;
-
-previous.addEventListener("click", () => swapImages("previous"));
-next.addEventListener("click", () => swapImages("next"));
-slider.addEventListener("touchstart", handleTouchStart);
-slider.addEventListener("touchmove", handleTouchMove);
-slider.addEventListener("touchend", handleTouchEnd);
-
-function swapImages(direction) {
-  if (direction === "next") {
-    currentPosition -= imageWidth;
-  } else if (direction === "previous") {
-    currentPosition += imageWidth;
+    slider.style.transform = `translate(${currentPosition}px, 0px)`;
   }
 
-  const lastImage =
-    window.innerWidth <= 768 ? totalImages - 1 : totalImages - 3;
-  const loopBackPosition = -(lastImage * imageWidth);
-
-  if (currentPosition < loopBackPosition) {
-    currentPosition = 0;
-  } else if (currentPosition > 0) {
-    currentPosition = loopBackPosition;
+  function handleTouchStart(event) {
+    touchStartX = event.touches[0].clientX;
   }
 
-  slider.style.transform = `translate(${currentPosition}px, 0px)`;
-}
+  function handleTouchMove(event) {
+    event.preventDefault();
 
-function handleTouchStart(event) {
-  touchStartX = event.touches[0].clientX;
-}
+    if (touchStartX === 0) return;
 
-function handleTouchMove(event) {
-  event.preventDefault();
+    const touchCurrentX = event.touches[0].clientX;
+    const touchDiff = touchStartX - touchCurrentX;
 
-  if (touchStartX === 0) return;
-
-  const touchCurrentX = event.touches[0].clientX;
-  const touchDiff = touchStartX - touchCurrentX;
-
-  if (touchDiff > 0) {
-    swapImages("next");
-  } else if (touchDiff < 0) {
-    swapImages("previous");
-  }
-
-  touchStartX = 0;
-}
-
-function handleTouchEnd() {
-  touchStartX = 0;
-}
-
-images.forEach((item, index) => {
-  let start = -950 + index * 25;
-  let end = -20 + index * 5;
-  gsap.fromTo(
-    item,
-    { x: 400, opacity: 0 },
-    {
-      opacity: 1,
-      x: 0,
-      scrollTrigger: {
-        trigger: item,
-        start: `${start}`,
-        end: `${end}`,
-        scrub: true,
-      },
+    if (touchDiff > 0) {
+      swapImages("next");
+    } else if (touchDiff < 0) {
+      swapImages("previous");
     }
-  );
+
+    touchStartX = 0;
+  }
+
+  function handleTouchEnd() {
+    touchStartX = 0;
+  }
+
+  previous.addEventListener("click", () => swapImages("previous"));
+  next.addEventListener("click", () => swapImages("next"));
+  slider.addEventListener("touchstart", handleTouchStart);
+  slider.addEventListener("touchmove", handleTouchMove);
+  slider.addEventListener("touchend", handleTouchEnd);
+
+  images.forEach((el, index) => {
+    let start = -750 + index * 60;
+    let end = -410 + index * 35;
+
+    slideFrom(el, 400, start, end);
+  });
 });
