7d8122ef7316d315ddb21a247db74de18bc2302d
Marina Grigoreva <mgrigoreva12@gmail.com>
2023-07-16 13:21:05 +0300

    Update section animation

--- js/script.js
+++ js/script.js
@@ -1,45 +1,44 @@
 gsap.registerPlugin(ScrollTrigger);
 
 function introAnimation() {
-  let tl = gsap.timeline({
-    defaults: {
-      opacity: 0,
-      duration: 0.7,
-      ease: "power4",
-    },
-  });
-
-  tl.from(".intro", { autoAlpha: 0 });
-  tl.from(".nav", { y: -100 });
-  tl.from("#img-first", {
-    rotate: "-15deg",
-    x: -100,
-    y: 100,
-    transformOrigin: "bottom left",
-    duration: 1,
-  });
-  tl.from(
-    "#img-second",
-    {
-      rotate: "15deg",
-      x: 100,
-      y: 100,
-      duration: 1,
-      transformOrigin: "bottom right",
-    },
-    "<"
-  );
-  tl.from(
-    "#img-third",
-    {
-      rotate: "25deg",
-      transformOrigin: "right",
-    },
-    "<0.3"
-  );
-
   ScrollTrigger.matchMedia({
     "(min-width: 992px)": function () {
+      let tl = gsap.timeline({
+        defaults: {
+          opacity: 0,
+          duration: 0.7,
+          ease: "power4",
+        },
+      });
+
+      tl.from(".intro", { autoAlpha: 0 });
+      tl.from(".nav", { y: -100 });
+      tl.from("#img-first", {
+        rotate: "-15deg",
+        x: -100,
+        y: 100,
+        transformOrigin: "bottom left",
+        duration: 1,
+      });
+      tl.from(
+        "#img-second",
+        {
+          rotate: "15deg",
+          x: 100,
+          y: 100,
+          duration: 1,
+          transformOrigin: "bottom right",
+        },
+        "<"
+      );
+      tl.from(
+        "#img-third",
+        {
+          rotate: "25deg",
+          transformOrigin: "right",
+        },
+        "<0.3"
+      );
       tl.from(
         ".intro-headings-name",
         {
@@ -59,6 +58,42 @@ function introAnimation() {
     },
 
     "(max-width: 991px)": function () {
+      let tl = gsap.timeline({
+        defaults: {
+          opacity: 0,
+          duration: 0.7,
+          ease: "power4",
+        },
+      });
+
+      tl.from(".intro", { autoAlpha: 0 });
+      tl.from(".nav", { y: -100 });
+      tl.from("#img-first", {
+        rotate: "-15deg",
+        x: -100,
+        y: 100,
+        transformOrigin: "bottom left",
+        duration: 1,
+      });
+      tl.from(
+        "#img-second",
+        {
+          rotate: "15deg",
+          x: 100,
+          y: 100,
+          duration: 1,
+          transformOrigin: "bottom right",
+        },
+        "<"
+      );
+      tl.from(
+        "#img-third",
+        {
+          rotate: "25deg",
+          transformOrigin: "right",
+        },
+        "<0.3"
+      );
       tl.from(
         ".intro-headings-name",
         {
@@ -79,18 +114,31 @@ function introAnimation() {
   });
 }
 
-// About
-// const textAnimation = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: ".smooth-headings",
-//     end: "top",
-//     scrub: 1,
-//     markers: true,
-//   },
-// });
-// textAnimation.from(".title", { xPercent: -100, duration: 0.7 });
-// textAnimation.from(".smooth-text", { xPercent: 500, duration: 1 });
+function sectionAnimation() {
+  const sections = document.querySelectorAll(".section");
+  sections.forEach((section) => {
+    let title = section.querySelector(".title");
+    let content = section.querySelector(".content");
+
+    let tl = gsap.timeline({ paused: true });
+    tl.from(title, { xPercent: -100, opacity: 0, duration: 0.7 });
+    tl.from(content, { yPercent: 100, opacity: 0, duration: 0.8 }, 0);
+
+    ScrollTrigger.create({
+      trigger: section,
+      start: "top 80%",
+      onEnter: () => tl.play(),
+    });
+
+    ScrollTrigger.create({
+      trigger: section,
+      start: "top 100%",
+      onLeaveBack: () => tl.pause(0),
+    });
+  });
+}
 
 window.onload = function () {
   introAnimation();
+  sectionAnimation();
 };
