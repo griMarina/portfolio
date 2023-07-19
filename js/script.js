c20c35d4b30e736052b62815a8d7a1acd40a3252
Marina Grigoreva <mgrigoreva12@gmail.com>
2023-07-19 12:32:06 +0300

    Update projects animation, add slider func

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ function introAnimation() {
       });
 
       tl.from(".intro", { autoAlpha: 0 });
-      tl.from(".nav", { y: -100 });
+      tl.from(".nav", { autoAlpha: 0, y: -100 });
       tl.from("#img-first", {
         rotate: "-15deg",
         x: -100,
@@ -67,7 +67,7 @@ function introAnimation() {
       });
 
       tl.from(".intro", { autoAlpha: 0 });
-      tl.from(".nav", { y: -100 });
+      tl.from(".nav", { autoAlpha: 0, y: -100 });
       tl.from("#img-first", {
         rotate: "-15deg",
         x: -100,
@@ -141,62 +141,116 @@ function sectionAnimation() {
 function projectsAnimation() {
   const projects = document.querySelectorAll(".projects-gallery figure");
 
-  ScrollTrigger.matchMedia({
-    "(min-width: 768px)": function () {
-      projects.forEach((element, index) => {
-        let items = element.querySelectorAll(".item");
-        let distance = index % 2 === 0 ? 100 : -100;
-        let tl = gsap.timeline({ paused: true });
+  projects.forEach((project) => {
+    const items = project.querySelectorAll(".item");
 
-        tl.from(items, {
-          duration: 0.5,
-          xPercent: gsap.utils.wrap([-distance, distance]),
-        });
-        tl.from(items, { opacity: 0, duration: 0.3 }, 0);
+    items.forEach((element) => {
+      const target = element.querySelector("div");
 
-        ScrollTrigger.create({
-          trigger: element,
-          start: "top 80%",
-          onEnter: () => tl.play(),
-        });
+      const tl = gsap.timeline({ paused: true });
 
-        ScrollTrigger.create({
-          trigger: element,
-          start: "top 100%",
-          onLeaveBack: () => tl.pause(0),
+      if (target.classList.contains("img")) {
+        tl.from(target, {
+          duration: 0.6,
+          opacity: 0,
+          scale: 0.5,
+          ease: "circ",
         });
+      } else {
+        tl.from(target, {
+          duration: 0.6,
+          opacity: 0,
+          y: 100,
+          ease: "circ",
+        });
+      }
+
+      ScrollTrigger.create({
+        trigger: project,
+        start: "top 50%",
+        onEnter: () => tl.play(),
       });
-    },
 
-    "(max-width: 767px)": function () {
-      gsap.utils.toArray(".item").forEach((element, index) => {
-        let target = element.querySelector("div");
-
-        let tl = gsap.timeline({ paused: true });
-        if (target.classList.contains("img")) {
-          tl.from(target, { duration: 0.5, opacity: 0, scale: 0.8 });
-        } else {
-          tl.from(target, { duration: 0.5, opacity: 0, y: 100 });
-        }
-
-        ScrollTrigger.create({
-          trigger: element,
-          start: "top 90%",
-          onEnter: () => tl.play(),
-        });
-
-        ScrollTrigger.create({
-          trigger: element,
-          start: "top 100%",
-          onLeaveBack: () => tl.pause(0),
-        });
+      ScrollTrigger.create({
+        trigger: project,
+        start: "top 100%",
+        onLeaveBack: () => tl.pause(0),
       });
-    },
+    });
   });
+}
+
+function sliderAnimation() {
+  const previous = document.querySelector(".slider-btn-previous");
+  const next = document.querySelector(".slider-btn-next");
+  const images = document.querySelectorAll(".slider-img");
+  const slider = document.querySelector(".slider-images");
+  const totalImages = images.length;
+  const imageWidth = images[0].clientWidth;
+  let currentPosition = 0;
+  let touchStartX = 0;
+
+  function swapImages(direction) {
+    const lastImgPosition = -(imageWidth * (totalImages - 1));
+
+    if (direction === "next") {
+      gsap.to(previous, { autoAlpha: 1 });
+
+      if (currentPosition > lastImgPosition) {
+        currentPosition -= imageWidth;
+      } else {
+        gsap.to(next, { autoAlpha: 0 });
+      }
+    } else if (direction === "previous") {
+      gsap.to(next, { autoAlpha: 1 });
+
+      if (currentPosition < 0) {
+        currentPosition += imageWidth;
+      } else {
+        gsap.to(previous, { autoAlpha: 0 });
+      }
+    }
+
+    gsap.to(slider, { x: currentPosition, duration: 0.7, ease: "power2" });
+  }
+
+  function handleTouchStart(event) {
+    touchStartX = event.touches[0].clientX;
+  }
+
+  function handleTouchMove(event) {
+    event.preventDefault();
+
+    if (touchStartX === 0) return;
+
+    const touchCurrentX = event.touches[0].clientX;
+    const touchDiff = touchStartX - touchCurrentX;
+
+    if (touchDiff > 0) {
+      swapImages("next");
+    } else if (touchDiff < 0) {
+      swapImages("previous");
+    }
+
+    touchStartX = 0;
+  }
+
+  function handleTouchEnd() {
+    touchStartX = 0;
+  }
+
+  previous.addEventListener("click", () => swapImages("previous"));
+  next.addEventListener("click", () => {
+    swapImages("next");
+  });
+  slider.addEventListener("touchstart", handleTouchStart);
+  slider.addEventListener("touchmove", handleTouchMove);
+  slider.addEventListener("touchend", handleTouchEnd);
 }
 
 window.onload = function () {
   introAnimation();
   sectionAnimation();
   projectsAnimation();
+  sliderAnimation();
 };
