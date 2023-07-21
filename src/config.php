3c5f6d3bb1b009131790375ff30b175797baad15
Marina Grigoreva <mgrigoreva12@gmail.com>
2023-07-21 14:33:30 +0300

    Remove src

--- src/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define("ROOT", dirname(__DIR__));
-
-define("EMAIL_USERNAME", "contact@grimarina.com");
-define("EMAIL_PASSWORD", "pubka1-jumhat-gichIn");
-
-include ROOT . "/src/vendor/autoload.php";
-include ROOT . "/src/send.php";
